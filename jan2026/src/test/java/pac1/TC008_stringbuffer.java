c1ac644d362aad2247779e468d5b2b39fa972d5a
Alziyan86 <alziyanahmad.it26@gmail.com>
2026-01-14 14:40:11 +0530

    done

---
+++ jan2026/src/test/java/pac1/TC008_stringbuffer.java
@@ -0,0 +1,15 @@
+package pac1;
+
+public class TC008_stringbuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				StringBuffer sb=new StringBuffer("Hello");
+				System.out.println(sb.reverse().insert(3, "----"));
+				
+				System.out.println(sb.append("   JAVA"));
+				
+			}
+	}
+
+
